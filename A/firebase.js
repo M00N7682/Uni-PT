c4e622f9d6ed11830a9336a62bba9c81f87b1423
m00n0__0 <dfjk71@khu.ac.kr>
2025-04-18 02:14:41 +0900

    Update firebase.js

--- A/firebase.js
+++ A/firebase.js
@@ -3,13 +3,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAb1k18Z57HtXChTGkS_LY7V66WpZh4SIk",
-  authDomain: "react-project-31495.firebaseapp.com",
-  projectId: "react-project-31495",
-  storageBucket: "react-project-31495.firebasestorage.app",
-  messagingSenderId: "525386895227",
-  appId: "1:525386895227:web:2cde7454a75a902fab77de",
-  measurementId: "G-VPNXG38WP6"
 };
 
 // Initialize Firebase
